15b2aa0d45a7289db9fd493695200b354cfd8f9d
kisphp <bogdan@kisphp.com>
2017-02-01 08:02:47 +0100

    finished methods

--- src/Db/Database.php
+++ src/Db/Database.php
@@ -3,6 +3,7 @@
 namespace Kisphp\Db;
 
 use Doctrine\DBAL\Driver\Connection;
+use Doctrine\DBAL\Driver\PDOStatement;
 
 class Database
 {
@@ -83,17 +84,83 @@ class Database
         );
 
         $stmt = $this->pdo->prepare($query);
+
+        $parameters = [];
         foreach ($tableFields as $key => $value) {
-            $stmt->bindParam(':' . $key, $value);
+            $parameters[':' . $key] = $value;
         }
-
         foreach ($conditions as $parameterName => $parameterValue) {
-            $stmt->bindParam(':' . $parameterName, $parameterValue);
+            $parameters[':' . $parameterName] = $parameterValue;
         }
 
-        $stmt->execute();
+        $stmt->execute($parameters);
 
         return $stmt->rowCount();
+    }
+
+    /**
+     * @param string $query
+     * @param array $conditions
+     *
+     * @return array
+     */
+    public function getPairs($query, array $conditions = [])
+    {
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute($conditions);
+
+        $output = [];
+        foreach ($stmt->fetchAll(\PDO::FETCH_NUM) as $row) {
+            $output[$row[0]] = $row[1];
+        }
+
+        return $output;
+    }
+
+    /**
+     * @param string $query
+     * @param array $conditions
+     *
+     * @return mixed
+     */
+    public function getValue($query, array $conditions = [])
+    {
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute($conditions);
+
+        $row = $stmt->fetch(\PDO::FETCH_NUM);
+
+        return $row[0];
+    }
+
+    /**
+     * @param string $query
+     * @param array $conditions
+     *
+     * @return array
+     */
+    public function getRow($query, array $conditions = [])
+    {
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute($conditions);
+
+        $row = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+        return $row;
+    }
+
+    /**
+     * @param string $query
+     * @param array $conditions
+     *
+     * @return \Doctrine\DBAL\Driver\Statement|PDOStatement
+     */
+    public function query($query, array $conditions = [])
+    {
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute($conditions);
+
+        return $stmt;
     }
 
     /**
@@ -105,8 +172,6 @@ class Database
     {
         $stmt = $this->pdo->prepare($query);
         $stmt->execute();
-
-//        $this->getLog()->log($stmt);
 
         return $stmt;
     }
